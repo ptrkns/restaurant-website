a8c9609336d1970c9ac0ffda1cede60334d9a7af
ptrkns <keneseipeterr@gmail.com>
2024-04-22 15:06:59 +0200

    Created the base of each component

---
+++ src/js/Home.js
@@ -0,0 +1,12 @@
+import '../css/Home.css';
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='Home-Parent'>
+      Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/js/Reservations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Reservations() {
+  return (
+    <div>
+      Reservations
+    </div>
+  )
+}
+
+export default Reservations;
