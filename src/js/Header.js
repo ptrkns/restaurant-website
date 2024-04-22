2afd280b76a69cc5e38448da45016d21a14473f4
ptrkns <keneseipeterr@gmail.com>
2024-04-22 18:53:46 +0200

    Made the 'Header' and 'Navbar' components responsive

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,14 +1,51 @@
 import '../css/Header.css';
-import logo from '../assets/logo.svg';
 import React from 'react'
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar.js';
+import logo from '../assets/logo.svg';
+import hamburger from '../assets/icon-hamburger-menu.svg';
 
 function Header() {
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [hamburgerClick, setHamburgerClick] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
+  const handleClick = () => {
+    setHamburgerClick(!hamburgerClick);
+  }
+
   return (
     <div className='Header-Parent'>
-        <Link to='/'><img src={logo}/></Link>
-        <Navbar/>
+        {windowWidth < 701 && <div className='Header-Container'>
+
+          <div className='Logo-Icon-Container'>
+            <Link to='/' onClick={()=>{setHamburgerClick(false)}}><img src={logo}/></Link>
+            <img src={hamburger} onClick={handleClick}/>
+          </div>
+
+          {hamburgerClick === true && <Navbar hamburgerClick={hamburgerClick} setHamburgerClick={setHamburgerClick}/>}
+
+        </div>}
+
+        {windowWidth >= 701 && <div className='Header-Container'>
+
+          <Link to='/'><img src={logo}/></Link>
+          <Navbar/>
+
+        </div>}
     </div>
   )
 }
