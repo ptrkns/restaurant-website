5eab8ad0cdcb5f49926badac7f0ac01294eaaf4f
ptrkns <keneseipeterr@gmail.com>
2024-04-22 15:29:27 +0200

    Created the 'Footer' component

---
+++ src/js/Footer.js
@@ -0,0 +1,61 @@
+import '../css/Footer.css';
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className='Footer-Parent'>
+      <div className='Footer-Container'>
+        <section>
+        <ul>
+            <li>London SW11 8AY, United Kingdom</li>
+            <li>+44 10 3333 4444</li>
+            <li>info@littlelemon.com</li>
+          </ul>
+        </section>
+        <section>
+          <table>
+            <tbody>
+              <tr>
+                <td className='Footer-td-left'>Monday</td>
+                <td className='Footer-td-right'>11 am - 11 pm</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Tuesday</td>
+                <td className='Footer-td-right'>11 am - 11 pm</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Wednesday</td>
+                <td className='Footer-td-right'>11 am - 11 pm</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Thursday</td>
+                <td className='Footer-td-right'>11 am - 11 pm</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Friday</td>
+                <td className='Footer-td-right'>11 am - 12 am</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Saturday</td>
+                <td className='Footer-td-right'>11 am - 12 am</td>
+              </tr>
+              <tr>
+                <td className='Footer-td-left'>Sunday</td>
+                <td className='Footer-td-right'>11 am - 10 pm</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <section>
+          <ul>
+            <li><a href='https://www.facebook.com' target='_blank'>Facebook</a></li>
+            <li><a href='https://www.instagram.com' target='_blank'>Instagram</a></li>
+            <li><a href='https://www.x.com' target='_blank'>X (Twitter)</a></li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
