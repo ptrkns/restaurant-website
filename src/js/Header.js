de71c790e3278bfd9a7f5e675baf2cfb19e9c93f
ptrkns <keneseipeterr@gmail.com>
2024-04-22 15:15:45 +0200

    Updated the name of the 'logo.svg' file

--- src/js/Header.js
+++ src/js/Header.js
@@ -1,5 +1,5 @@
 import '../css/Header.css';
-import logo from '../assets/Logo.svg';
+import logo from '../assets/logo.svg';
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar.js';
