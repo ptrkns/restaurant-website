5f6c56b6428f9ecd5d84a432fc28d43d1dcb20ba
ptrkns <keneseipeterr@gmail.com>
2024-04-24 16:48:06 +0200

    Updated the classNames

--- src/js/Home.js
+++ src/js/Home.js
@@ -14,24 +14,24 @@ function Home() {
 
   return (
     <div className='Home-Parent'>
-      <article className='Hero-Background'>
-        <div className='Hero-Container'>
-          <section className='Hero-Container-Left'>
-              <h1>Little Lemon</h1>
-              <h2>Chicago</h2>
-              <p>
-                  We are a family owned Mediterranean restaurant, 
-                  focused on traditional recipes served with 
-                  a modern twist
-              </p>
-              <Link className='Link-Button' to="/reservations">Reserve a Table</Link>
-          </section>
-          <section className='Hero-Container-Right'>
-              <img src={food} alt='Hero image'/>
-          </section>
-        </div>
+    <article className='Hero-Background'>
+      <div className='Hero-Article'>
+        <section className='Hero-Article-Left'>
+            <h1>Little Lemon</h1>
+            <h2>Chicago</h2>
+            <p>
+                We are a family owned Mediterranean restaurant, 
+                focused on traditional recipes served with 
+                a modern twist
+            </p>
+            <Link className='Link-Button' to="/reservations">Reserve a Table</Link>
+        </section>
+        <section className='Hero-Article-Right'>
+            <img src={food} alt='Hero image'/>
+        </section>
+      </div>
     </article>
-    <article className='Highlights-Container'>
+    <article className='Highlights-Article'>
       <section className='Highlights-Top'>
           <h1>This weeks specials!</h1>
           <Link className='Link-Button' to="/order">Order online</Link>
@@ -39,14 +39,14 @@ function Home() {
       <section className='Highlights-Bottom'>
       </section>
     </article>
-    <article className='About-Container'>
+    <article className='About-Article'>
       <section className='About-Left'>
-          <h3>About us</h3>
+          <h1>About us</h1>
           <p>{loremIpsum}</p>
       </section>
       <section className='About-Right'>
-          <img src="" alt='Mario and Adrian' className='img1'/>
-          <img src="" alt='Mario and Adrian' className='img2'/>
+          <img src={marioAdrian1} alt='Mario and Adrian' className='img1'/>
+          <img src={marioAdrian2} alt='Mario and Adrian' className='img2'/>
       </section>
     </article>
     </div>
